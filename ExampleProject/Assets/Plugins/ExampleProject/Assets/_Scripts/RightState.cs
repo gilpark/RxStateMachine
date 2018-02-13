d167e2633833669468d1e05df435bb53902da334
sendtogil <sendtogil@gmail.com>
2018-02-12 22:54:51 -0600

    Add 'ExampleProject/Assets/Plugins/' from commit '615eeb34e82e1c59160a0d44c2a6a8be9bdc7087'
    git-subtree-dir: ExampleProject/Assets/Plugins
    git-subtree-mainline: 615eeb34e82e1c59160a0d44c2a6a8be9bdc7087
    git-subtree-split: 615eeb34e82e1c59160a0d44c2a6a8be9bdc7087

---
+++ ExampleProject/Assets/Plugins/ExampleProject/Assets/_Scripts/RightState.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using Bebimbop.Utilities;
+using Bebimbop.Utilities.StateMachine;
+using UnityEngine;
+
+namespace Bebimbop.Example
+{
+	public class RightState : MonoBehaviour
+	{
+		public CanvasGroup[] UiPanels;
+		private StateMachine<Right> _rightStateMachine;
+
+		private void Start () 
+		{
+			_rightStateMachine = StateMachine<Right>.Initialize(this);
+			UiPanels.ToList().ForEach(x => x.DisableCanvasGroup(true));
+			_rightStateMachine.SetMode(StateTransition.Overwrite);
+			_rightStateMachine.ChangeState(Right.One);
+		}
+
+		public void ChangeState(int idx)
+		{
+			_rightStateMachine.ChangeState((Right)idx);
+		}
+
+		//we done need Exit method since this example's transition mode is overwrite
+		//but we use OnCancel method to reset canvas group objects
+		private void One_Enter()
+		{
+			UiPanels[0].FadingIn(1);
+		}
+		private void One_OnCancel()
+		{
+			UiPanels[0].DisableCanvasGroup(true);
+		}
+
+		
+		private void Two_Enter()
+		{
+			UiPanels[1].FadingIn(1);
+		}
+		private void Two_OnCancel()
+		{
+			UiPanels[1].DisableCanvasGroup(true);
+		}
+		
+		
+		private void Three_Enter()
+		{
+			UiPanels[2].FadingIn(1);
+		}
+		private void Three_OnCancel()
+		{
+			UiPanels[2].DisableCanvasGroup(true);
+		}
+		
+		private enum Right
+		{
+			One = 0,Two,Three
+		}
+	}
+}
+
